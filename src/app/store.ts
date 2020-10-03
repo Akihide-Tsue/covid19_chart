4794e8b25103aa6527332edcb25d1011e7fd02f5
Akihide Tsue <tsue_akihide@emplayee.co.jp>
2020-10-03 10:13:54 +0900

    不要な記述を削除、リファクタリング

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,9 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 
+import covidReducer from "../features/covid/covidSlice";
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    covid: covidReducer,
   },
 });
 
