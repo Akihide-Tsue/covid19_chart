e98ab56e2a3e25a650938d7af6d5b3938b60ff3a
Akihide Tsue <tsue_akihide@emplayee.co.jp>
2020-10-03 10:43:16 +0900

    ダッシュボードにコメント追記

--- src/features/covid/SwitchCountry/SwitchCountry.tsx
+++ src/features/covid/SwitchCountry/SwitchCountry.tsx
@@ -16,30 +16,37 @@ const SwitchCountry: React.FC = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
 
+  //TODO:地域（アジア、北米など）で分けた場合も作成
   const countries = [
     "japan",
-    "china",
     "us",
-    "france",
-    "italy",
-    "spain",
-    "united kingdom",
-    "germany",
-    "russia",
     "brazil",
+    "china",
+    "france",
+    "germany",
+    "india",
+    "italy",
+    "new zealand",
+    "russia",
+    "spain",
+    "sweden",
     "taiwan",
     "thailand",
-    "new zealand",
-    "sweden",
-    "india",
+    "united kingdom",
   ];
+  const style = { marginBottom: "15px" };
 
   return (
     <FormControl className={classes.formControl}>
+      <div>13:00-15:00頃にAPIのメンテナンス中の場合は利用できないことがあります。</div>
+      <div style={style}>
+        API : <a href="https://documenter.getpostman.com/view/10808728/SzS8rjbc">
+          https://documenter.getpostman.com/view/10808728/SzS8rjbc
+        </a>
+      </div>
+
       <NativeSelect
-        onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
-          dispatch(fetchAsyncGetDaily(e.target.value))
-        }
+        onChange={(e: React.ChangeEvent<HTMLSelectElement>) => dispatch(fetchAsyncGetDaily(e.target.value))}
       >
         {countries.map((country, i) => (
           <option key={i} value={country}>
